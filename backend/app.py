de87610a6f10b15123b3b55bd6cdd6db6f727353
Magnus Gao <MAGNUSGAO2@GMAIL.COM>
2021-08-24 12:36:26 -0400

    First version before debugging

--- backend/app.py
+++ backend/app.py
@@ -51,9 +51,6 @@ def mirror(name):
     data = {"name": name}
     return create_response(data)
 
-@app.route("/shows", methods=['GET'])
-def get_all_shows():
-    return create_response({"shows": db.get('shows')})
 
 @app.route("/shows/<id>", methods=['DELETE'])
 def delete_show(id):
@@ -64,6 +61,69 @@ def delete_show(id):
 
 
 # TODO: Implement the rest of the API here!
+
+# Does the order matter?
+# Learned: <x> allows us to pass in a as an argument
+@app.route("/shows/<id>", methods=['GET'])
+def get_a_show(id):
+    if db.getById('shows', int(id)) is None:
+        return create_response(status=404, message="Sorry show with this ID Does not exist, yet.")
+    else:
+        return create_response({"shows": db.getById('shows', int(id))})
+
+@app.route("/shows", methods=['POST'])
+def post_a_show():
+    # Error check
+    if not "name" in request.json:
+        return create_response(status=422, message="No show name provided")
+    elif not "episodes_seen" in request.json:
+        return create_response(status=422, message="No Episode watched information provided")
+
+    request.json["episodes_seen"] = int(request.json["episodes_seen"])
+    
+    db_response = db.create("shows", request.json)
+    return create_response(db_response, status=201, message="Successfully inputted show")
+
+
+# I read partial updates should use PATCH
+@app.route("/shows/<id>", methods=['PUT'])
+def update_a_show(id):
+    # If id cannot be found
+    if db.getById('shows', int(id)) is None:
+        return create_response(status=404, message="id not found")
+    if not "name" in request.json:
+        return create_response(status=422, message="No show name provided")
+    elif not "episodes_seen" in request.json:
+        return create_response(status=422, message="No Episode watched information provided")
+
+    # Create a new json object with two values only
+    temp_json = {}
+    temp_json["name"] = request.json["name"]
+    temp_json["episodes_seen"] = int(request.json["episodes_seen"])
+    
+    db_response = db.updateById("shows", int(id), temp_json)
+    return create_response(db_response, status=201, message="Successfully updated show")
+   
+@app.route("/shows", methods=['GET'])
+def hello():
+    if request.args.get('minEpisodes') is not None:
+        return get_shows_with_minEpisodes()
+    else:
+        return get_all_shows()
+
+def get_all_shows():
+    return create_response({"shows": db.get('shows')})
+
+def get_shows_with_minEpisodes():
+    min_episodes = int(request.args.get('minEpisodes'))
+    all_shows = db.get("shows")
+    temp_json = {}
+    temp_json["shows"] = []
+
+    for show in all_shows:
+        if show["episodes_seen"] >= min_episodes:
+            temp_json["shows"].append(show)
+    return create_response(temp_json, status=201, message="Successfully retrieved shows")
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
